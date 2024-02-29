47a6d59e3328cb70c3ed5acf99f5057e14109556
burcakyildirim <brckyldrm@gmail.com>
2024-02-29 23:45:11 +0300

    DELETE added

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import './Category.css'
 
 function Category() {
   const [category, setCategory] = useState([]);
@@ -13,12 +14,22 @@ function Category() {
       });
   }, []);
 
+  const handleDelete = (event) => {
+    const id = event.target.id;
+    console.log(id);
+    fetch(`http://localhost:8080/v1/categories/${id}`, {
+      method: "DELETE",
+    }).catch((error) => {
+      console.error("Error: ", error)
+    })
+  }
+
   return (
     <>
       <h1>Category</h1>
-      <div>
+      <div className="list">
         {category.map((category) => (
-          <div key={category.id}>{category.name}</div>
+          <div id = {category.id} onClick = {(e) => handleDelete(e)} key={category.id}>{category.name}</div>
         ))}
       </div>
     </>
