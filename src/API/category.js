dffb1de96835f6391e8e71afd549c6d47fbd154e
burcakyildirim <brckyldrm@gmail.com>
2024-03-02 01:07:33 +0300

    .env ADDED

--- src/API/category.js
+++ src/API/category.js
@@ -1,22 +1,26 @@
 import axios from "axios";
 
 export const getCategories = async () => {
-    const {data} = await axios.get("http://localhost:8080/v1/categories")
+    const {data} = await axios.get(
+        import.meta.env.VITE_APP_BASE_URL + "/v1/categories")
     return data;
 }
 
 export const deleteCategory = async (id) => {
-    const {data} = await axios.delete(`http://localhost:8080/v1/categories/${id}`)
+    const {data} = await axios.delete(
+        import.meta.env.VITE_APP_BASE_URL + `/v1/categories/${id}`)
     return data;
 }
 
 export const createCategory = async (categories) => {
-    const {data} = await axios.post(`http://localhost:8080/v1/categories`, categories)
+    const {data} = await axios.post(
+        import.meta.env.VITE_APP_BASE_URL + `/v1/categories`, categories)
     return data;
 }
 
 export const updateCategoryAPI = async (categories) => {
-    const {data} = await axios.put(`http://localhost:8080/v1/categories`, categories)
+    const {data} = await axios.put(
+        import.meta.env.VITE_APP_BASE_URL + `/v1/categories`, categories)
     //eğer id ile update yapıyorsam `http://localhost:8080/v1/categories/${categories.id}`
     return data;
 }
--- src/API/publisher.js
+++ src/API/publisher.js
@@ -1,22 +1,26 @@
 import axios from "axios";
 
 export const getPublishers = async () => {
-    const {data} = await axios.get("http://localhost:8080/v1/publishers")
+    const {data} = await axios.get(
+        import.meta.env.VITE_APP_BASE_URL + "/v1/publishers")
     return data;
 }
 
 export const deletePublishers = async (id) => {
-    const {data} = await axios.delete(`http://localhost:8080/v1/publishers/${id}`)
+    const {data} = await axios.delete(
+        import.meta.env.VITE_APP_BASE_URL + `/v1/publishers/${id}`)
     return data;
 }
 
 export const createPublishers = async (publishers) => {
-    const {data} = await axios.post(`http://localhost:8080/v1/publishers`, publishers)
+    const {data} = await axios.post(
+        import.meta.env.VITE_APP_BASE_URL + `/v1/publishers`, publishers)
     return data;
 }
 
 export const updatePublishersAPI = async (publishers) => {
-    const {data} = await axios.put(`http://localhost:8080/v1/publishers`, publishers)
+    const {data} = await axios.put(
+        import.meta.env.VITE_APP_BASE_URL + `/v1/publishers`, publishers)
     //eğer id ile update yapıyorsam `http://localhost:8080/v1/publishers/${publishers.id}`
     return data;
 }
