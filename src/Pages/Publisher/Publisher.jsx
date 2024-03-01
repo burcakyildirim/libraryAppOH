87aae7fbf53069fa2ff90d8e30f811f6f5ec3b4e
burcakyildirim <brckyldrm@gmail.com>
2024-03-02 00:56:04 +0300

    ADDED publisher done

--- src/Pages/Publisher/Publisher.jsx
+++ src/Pages/Publisher/Publisher.jsx
@@ -12,6 +12,14 @@ import "./Publisher.css";
 function Publisher() {
   const [publisher, setPublisher] = useState([]);
   const [reload, setReload] = useState(true);
+  const [newPublisher, setNewPublisher] = useState({
+    name: "",
+    establishmentYear: "",
+  });
+  const [updatePublisher, setUpdatePublisher] = useState({
+    name: "",
+    establishmentYear: "",
+  });
 
   useEffect(() => {
     getPublishers().then((data) => {
@@ -20,14 +28,106 @@ function Publisher() {
     setReload(false);
   }, [reload]);
 
+  const handleNewPublisher = (event) => {
+    setNewPublisher({
+      ...newPublisher,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleCreate = () => {
+    createPublishers(newPublisher).then(() => {
+      setReload(true);
+    });
+    setNewPublisher({
+      name: "",
+      establishmentYear: "",
+    });
+  };
+
+  const handleUpdateChange = (event) => {
+    setUpdatePublisher({
+      ...updatePublisher,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleUpdate = () => {
+    updatePublishersAPI(updatePublisher).then(() => {
+      setReload(true);
+    });
+    setUpdatePublisher({
+      name: "",
+      establishmentYear: "",
+    });
+  };
+  const handleDelete = (id) => {
+    //deleteCategory js kısmına ekledik ordan çektik
+    //const id = event.target.id;
+    deletePublishers(id).then(() => {
+      setReload(true);
+    });
+  };
+  const handleUpdateBtn = (pub) => {
+    setUpdatePublisher({
+      name: pub.name,
+      establishmentYear: pub.establishmentYear,
+      id: pub.id,
+    });
+  };
   return (
     <>
+      <h1>Publisher</h1>
+      <div className="publisher-newpublisher">
+        <h2>Yeni Publisher</h2>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={newPublisher.name}
+          onChange={handleNewPublisher}
+        />
+        <input
+          type="number"
+          placeholder="establishmentYear"
+          name="establishmentYear"
+          value={newPublisher.establishmentYear}
+          onChange={handleNewPublisher}
+        />
+        <button onClick={handleCreate}>Create</button>
+      </div>
+      <div className="publisher-updatepublisher">
+        <h2>Yayımcı Güncelle</h2>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          onChange={handleUpdateChange}
+          value={updatePublisher.name}
+        />
+        <input
+          type="text"
+          placeholder="establishmentYear"
+          name="establishmentYear"
+          onChange={handleUpdateChange}
+          value={updatePublisher.establishmentYear}
+        />
+        <button onClick={handleUpdate}>Update</button>
+      </div>
       <div className="list">
-        <h2>Kategori Listesi</h2>
+        <h2>Yayımcı Listesi</h2>
         {publisher.map((publishers) => (
           <div key={publishers.id}>
-            <h3>{publishers.name} {publishers.id}</h3>{publishers.establishmentYear}
-            
+            <h3>
+              {publishers.name}
+              <span id={publishers.id} onClick={() => handleDelete(publishers.id)}>
+                <DeleteIcon />
+              </span>
+              <span onClick={() => handleUpdateBtn(publishers)}>
+                <UpdateIcon />
+              </span>
+            </h3>
+            {publishers.establishmentYear}
           </div>
         ))}
       </div>
